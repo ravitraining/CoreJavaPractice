0c0edb4824a4b771b407141a27d05b3c5c5e11af
ravitraining <raviwalture@gmail.com>
2019-11-05 09:29:04 +0530

    New Exception Handling

--- CoreJava/ExceptionHandling/CheckingCustomizedException.java
+++ CoreJava/ExceptionHandling/CheckingCustomizedException.java
@@ -1,22 +1,16 @@
 package ExceptionHandling;
 
 public class CheckingCustomizedException {
-public static void main(String[] args) {
-	int age=441;
-	if(age<18)
-	{
-		int age1=18-age;
-		throw new TooYoungException();
-
+	public static void main(String[] args) {
+		int age = 441;
+		if (age < 18) {
+			int age1 = 18 - age;
+			throw new TooYoungException();
+		} else if (age > 60) {
+			int age1 = age - 60;
+			throw new TooOldExceptoin();
+		} else {
+			System.out.println("Welcome to Election booth");
+		}
 	}
-	else if(age>60)
-	{
-		int age1=age-60;
-		throw new TooOldExceptoin();
-	}
-	else
-	{
-		System.out.println("Welcome to Election booth");
-	}
-}
 }
